bec2389ad2527c4df71861524bf3315c03b5f344
vimukthi-nm2309242 <nm2309242@qu.edu.qa>
2025-03-31 15:07:46 +0300

    modified login page

--- loginpage/script.js
+++ loginpage/script.js
@@ -1,17 +1,67 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-
-  // const form = document.querySelector("login-form");
-  // const username = document.querySelector("username");
-  // const pw = document.querySelector("password");
-  // const type = document.querySelector("form-input").value;
+  const user = document.querySelector("#username");
+  const pw = document.querySelector("#password");
+  const typeOF = document.querySelector("#userType");
   const signin = document.querySelector("#submitButton");
 
+  const usernameError = document.querySelector("#username-error");
+  const passwordError = document.querySelector("#password-error");
+  const typeError = document.querySelector("#type-error");
+  const loginError = document.querySelector("#login-error");
 
-  signin.addEventListener("click", (e) => {
+
+
+  signin.addEventListener("click", async (e) => {
     e.preventDefault();
-    window.location.href = "../pages/studentdashboard.html";
+
+    const username = user.value.trim();
+    const password = pw.value.trim();
+    const type = typeOF.value.trim();
+
+    usernameError.textContent = "";
+    passwordError.textContent = "";
+    loginError.textContent = "";
+
+    let hasError = false;  //this acts as a flag so itll reset the entire login page
+
+
+    if(!username ||!password){
+      loginError.textContent = "Fill in all fields.";
+      hasError = true;
+    }
+    if (!username) {
+      loginError.textContent = "Username is required.";
+      hasError = true;
+    }
+    if (!password) {
+      loginError.textContent = "Password is required.";
+      hasError = true;
+    }
+    if (hasError) return;
+
+    const response = await fetch("users.json");
+    const users = await response.json();
+
+    const detail = users.find(u => 
+      u.username === username &&
+      u.password === password &&
+      u.type === type
+    );
+
+    if(detail){
+      if(type==="student"){
+        window.location.href = "../student_pages/studentdashboard.html";
+      }
+      else if(type==="instructor"){
+        window.location.href="https://www.youtube.com/";
+      }
+      else if(type==="admin"){
+        window.location.href="https://www.google.com/";
+      }
+    }
+    else{
+      loginError.textContent = "Invalid username, password, or type.";
+    }    
   });
-
-
 });
