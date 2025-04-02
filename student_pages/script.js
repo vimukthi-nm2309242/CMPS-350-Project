469a4fefc17b964848ffe3362a075fdeae2d0b89
vimukthi-nm2309242 <nm2309242@qu.edu.qa>
2025-04-02 16:21:35 +0300

    implemented a prerequisite check for validations but need to complete the registration process and display appropriate message for each validations

--- student_pages/script.js
+++ student_pages/script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     let allCourses = [];
 
+    let currentUser = localStorage.getItem('currentUser');
+    let currentStudent;
+
     async function courses() {
         const response = await fetch("courses.json");
         if (response.ok) {
@@ -8,17 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
             displayCourses(allCourses);     //passes all the courses objects to the display courses function
             
             filter(searchbar.value);     //this function allows use to input a value into the search field to filter stuff
+
+            // console.log(allCourses)
         }
     }
-    // async function images(){
-    //     const imgres = await fetch("https://picsum.photos/200/300");
-    //     let img;
-    //     if(imgres.ok){
-    //         img = await imgres.blob();
-    //     }
-
-    //     return URL.createObjectURL(img)
-    // }
 
     function displayCourses(courses) {
         const container = document.querySelector(".container");
@@ -28,27 +24,37 @@ document.addEventListener("DOMContentLoaded", function () {
             container.innerHTML = "No courses Available";
             return;
         }
-
         courses.forEach(course => {
             const courseHTML = document.createElement("div");
             courseHTML.classList.add("classes");
 
-            // const image = await images();
+            const registerBtn = document.createElement("button");
+            registerBtn.textContent = "Register";
+            registerBtn.dataset.courseId = course.id;
+
+            registerBtn.addEventListener("click", async () => {
+                
+                const result = await prerequisiteCheck(course.id);
+
+                console.log(result.message);
+            });  
 
             courseHTML.innerHTML = `
                 <div class="items">
                     <div class="desc">
                         <h3>${course.code}</h3>
                         <p>${course.name}</p>
-                        <p>${course.description}</p>
-                        <p>${course.instructor}</p>
+                        <p>Instructor: ${course.instructor ?? "TBD"}</p>
+                        <p>Available seats: ${course.availableSeats}</p>
+                        <p>Prerequisites: ${course.prerequisites.length > 0 ? course.prerequisites.join(", ") : "None"}</p>
+
                     </div>
                     <div class="register-btn">
-                        <button>Register</button>
                     </div>
                 </div>
             `;
 
+            courseHTML.querySelector(".register-btn").appendChild(registerBtn);
             container.appendChild(courseHTML);
 
             // in the student dashboard HTML there is a boiler template that is created 
@@ -73,7 +79,6 @@ document.addEventListener("DOMContentLoaded", function () {
         //this filtered variable filters the courses based on what the user haas entered
         //watched youtube tutorials to figure this one out
 
-
         displayCourses(filtered);   //then we render out the filtered version
     }
 
@@ -84,6 +89,67 @@ document.addEventListener("DOMContentLoaded", function () {
     searchbtn.addEventListener("click", () => {
         filter(searchbar.value);            
     })
+
+    async function retrieveUser(){
+        const response = await fetch("../loginpage/users.json");
+        if(response.ok){
+            const users = await response.json();
+
+            return users.find(user => user.username === currentUser);
+        }
+        return null;
+    }
+
+    async function prerequisiteCheck(courseId){
+        const course = allCourses.find(course => course.id === courseId);   
+        //when user clicks on the register button it finds this particular course 
+        
+        const user = await retrieveUser(); //get logged in user information
+
+        console.log(user);
+
+        if(!course.openForRegistration){
+            return {
+                success: false,
+                message: "Course is not Open for registration"
+            };
+        }
+
+        if(course.availableSeats <=0){
+            return {
+                success: false,
+                message: "Course is full"
+            };
+        }
+        
+
+        if(course.prerequisites.length === 0){
+            return {
+                success: true,
+                message: "Course can beregistered, no prerequisites"
+            };
+        }
+
+        const missingPrerequisites = course.prerequisites.filter(c => !user.completedCourses.includes(c));
+        //this variable filters out the prerequisities that the current user has not completed
+        //it searches through the completed courses and if it does not include that particular prereq
+        //then it adds it into the missingprereq array
+
+        if(missingPrerequisites.length > 0){
+            return {
+                success: false,
+                message: `You need to complete ${missingPrerequisites.join(", ")} before registering for ${course.id}`
+            }
+        }
+
+        return{
+            success: true,
+            message: "All prerequisites are met."
+        }
+
+        //above are few basic validations just to see if there are seats or if the class is open or if there is no prerequisites
+
+    }
 
     courses();    //call the course function to allow the courses to load
 });
